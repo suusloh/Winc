19f9a81fdb1e2bd9f66ad015ffadb7c3ab59bf4d
suusloh <130738896+suusloh@users.noreply.github.com>
2023-05-21 13:01:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# Do not modify these lines
+__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
+__human_name__ = 'operators'
+
+# Add your code after this line
+
+# Language spoken most Switserland same as Spain
+spain_language = 'Castilan Spanish'
+switzerland_language = 'Swiss German'
+print(spain_language == switzerland_language)
+
+# Most Prevalent Religion in Spain same as in Switserland
+spain_religion = 'Roman Catholic'
+switzerland_religion = 'Roman Catholic'
+print(spain_religion == switzerland_religion)
+
+# Spain capital name lenght does not equal of Switserlands capital name
+len_spain_capital = len('Madrid')
+len_switzerland_capital = len('Bern')
+print(len_spain_capital != len_switzerland_capital)
+
+# Switserland GDP is greater dan than Spain GDP
+spain_GDP = 1393351000000
+switzerland_GDP = 731502000000000
+print(switzerland_GDP < spain_GDP)
+
+# Population growth Spain and Switseland less than 1%
+spain_population_growth = 0.13
+switzerland_population_growth = 0.65
+print(spain_population_growth < switzerland_population_growth)
+
+# Spain population count
+spain_population_count = 47163418
+print(spain_population_count >= 10000000)
+
+# Switzerland population count
+switzerland_population_count = 8508698
+print(switzerland_population_count <= 10000000)
+
+
+
+
